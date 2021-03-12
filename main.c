64cbb69a9953633641c180250d0d196dc3884a66
tim <405362140@qq.com>
2021-03-12 11:50:28 +0800

    change para

--- main.c
+++ main.c
@@ -105,7 +105,7 @@ int main(int argc, char *argv[])
 
     //创建数据库连接池
     connection_pool *connPool = connection_pool::GetInstance();
-    connPool->init("localhost", "root", "root", "qgydb", 3306, 8);
+    connPool->init("localhost", "root", ":Timso1997", "mydb", 3306, 8);
 
     //创建线程池
     threadpool<http_conn> *pool = NULL;
